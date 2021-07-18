2715d3fddba2aeb8111293d3f97a7dacfbd9e620
Edwin Troya <edtroyajr@gmail.com>
2021-07-18 16:17:32 -0700

    Lovely seats

---
+++ Receipts.py
@@ -0,0 +1,37 @@
+lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
+lovely_loveseat_price = 254.00
+
+stylish_settee_description ="Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
+#price
+stylish_settee_price = 180.50
+#item lamp
+luxurious_lamp_description = "Luxurious Lamp. Glass and iron. 36 inches tall. Brown with cream shade."
+#price of the lamp
+luxurious_lamp_price= 52.15
+
+#8.8% sales taxes is written like this.
+sales_tax = .088
+
+#first customer is making their purchase. 0 at first.
+customer_one_total = 0
+
+#Variabvle of description of things they're purchasing
+customer_one_itemization =""
+#Adding the customer total with price of the item. 
+customer_one_total += lovely_loveseat_price
+
+customer_one_itemization += lovely_loveseat_description
+
+customer_one_total += luxurious_lamp_price
+
+customer_one_itemization += luxurious_lamp_description
+
+customer_one_tax =customer_one_total * sales_tax
+
+customer_one_total + customer_one_tax
+
+print("Customer One Items")
+print(customer_one_itemization)
+
+print("Customer One Total")
+print(customer_one_total)
